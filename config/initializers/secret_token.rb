98f337f6517083020c26856c872786c563ed0b39
John Moon <johnmoonyy@gmail.com>
2013-10-06 14:13:18 -0400

    Build and Break File

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Failfree::Application.config.secret_key_base = 'b68ead995fdce44d048b7fe0a1f9eee869164d5aeefdda7e27068e6ee87c553faff73461dd2cc94f30884a1a2ed9cda3622375459ca6cd32d36203f2a246b7eb'
